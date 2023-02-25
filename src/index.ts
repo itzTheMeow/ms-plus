fcc106305a424d59bc6ea572b88c4141a5382a0a
Meow <me@itsmeow.cat>
2023-02-24 22:05:02 -0500

    v0.0.3 - fix typings

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ export * from "./types";
 /** Returns an object allowing formatting of the parsed millisecond or string input. Returns `null` if the input is invalid. */
 export default function ms(
   val: number | string
-): { toString(): string; toNumber(): number } | null {
+): { toString(opts?: FormatMSOptions): string; toNumber(): number } | null {
   const units = toUnits(val);
   if (!units.length) return null;
   return {
-    toString(opts?: FormatMSOptions) {
+    toString(opts) {
       return toString(units, opts);
     },
     toNumber() {
