3abfb502db65de95a65e985e2aaaf2c602e4bb83
Meow <me@itsmeow.cat>
2023-04-10 13:37:16 -0400

    v0.0.5 - Add parser.drop()

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,49 @@ import { FormatMSOptions } from "./types";
 
 export * from "./types";
 
+export interface MSParser {
+  /**
+   * Converts the input to a string.
+   * @param opts Optional settings to tweak the output.
+   */
+  toString(opts?: FormatMSOptions): string;
+  /** Converts the input to a number. */
+  toNumber(): number;
+  /**
+   * Removes any unit matching the input number.
+   * @param unit The unit value to remove.
+   *
+   * @example
+   * ```ts
+   * const units = ms("10m5s80ms");
+   * // => 10 minutes, 5 seconds, 80 milliseconds
+   *
+   * ms.drop(1000);
+   * // => 10 minutes, 5 seconds
+   *
+   * ms.drop(ms("1 minute").toNumber());
+   * // => 5 seconds
+   * ```
+   */
+  drop(unit: number): MSParser;
+}
+
 /** Returns an object allowing formatting of the parsed millisecond or string input. Returns `null` if the input is invalid. */
-export default function ms(
-  val: number | string
-): { toString(opts?: FormatMSOptions): string; toNumber(): number } | null {
+export default function ms(val: number | string): MSParser | null {
   const units = toUnits(val);
   if (!units.length) return null;
-  return {
+  const parser: MSParser = {
     toString(opts) {
       return toString(units, opts);
     },
     toNumber() {
       return toNumber(units);
     },
+    drop(dropped) {
+      const i = units.findIndex((u) => u.value == dropped);
+      if (i >= 0) units.splice(i, 1);
+      return parser;
+    },
   };
+  return parser;
 }
